557b5a29aacf179e1cf17c1fd180820d33a20dd0
Joan Torres Lopez <joantolo@redhat.com>
2026-02-12 17:36:08 +0100

    authPrompt: Show entry area when displaying message
    Since entryArea is hidden by default, we must explicitly make it
    visible when showing a message if no other widgets are visible.
    This allows getting a preemptive answer.

--- js/gdm/authPrompt.js
+++ js/gdm/authPrompt.js
@@ -522,6 +522,15 @@ export const AuthPrompt = GObject.registerClass({
         }
 
         this.setMessage(message, type, wiggleParameters);
+
+        // If we're showing a message and no auth widget is currently visible,
+        // show the entry area to allow getting a preemptive answer
+        if (message &&
+            type < GdmUtil.MessageType.ERROR &&
+            !this._entryArea.visible &&
+            !this._authList.visible)
+            this._fadeInElement(this._entryArea);
+
         this.emit('prompted');
     }
 
